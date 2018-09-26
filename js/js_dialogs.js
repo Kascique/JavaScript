2203163d56a11c92b3766e22ef73b978230d5a85
Kascique Lowmans <Kascique@users.noreply.github.com>
2018-09-26 17:17:25 -0400

    JavaScript Alerts

---
+++ js/js_dialogs.js
@@ -0,0 +1,8 @@
+/*can be use to collect user input*/
+var user_name = prompt("What is your name?", "");
+
+/*can be use to confirm actions*/
+var user_confirm = confirm("Do you like Ice cream ?");
+
+/*can be use to show messages to user*/
+alert("Basic JS Alert");
